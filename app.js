18ce2557576bb5135ef6352ee4b5ee5c9f5eea9b
Nama Kamu <email-github-kamu@example.com>
2026-03-03 10:45:38 +0700

    perbaikan port untuk railway

--- app.js
+++ app.js
@@ -116,5 +116,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log('SERVER RUNNING...');
+    console.log('SERVER RUNNING ON PORT ' + PORT);
 });
